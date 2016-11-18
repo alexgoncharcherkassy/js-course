d069c435db50d598a386abd80ced038a078a1bed
Oleksandr Gonchar <cs210785gaa@gmail.com>
2016-11-18 15:38:34 +0200

    homework 3

---
+++ homeworks/oleksandr.gonchar_alexgoncharcherkassy/homework_3/js/main.js
@@ -0,0 +1,81 @@
+var originalSetTimeout = window.setTimeout;
+
+window.setTimeout = function (delay, callback) {
+    if (typeof callback != 'function') {
+        throw new TypeError();
+    }
+    if (typeof delay != 'number') {
+        throw new TypeError();
+    }
+    var aArgs = arguments;
+
+    return originalSetTimeout(function () {
+        callback.apply(this, aArgs);
+    }, delay);
+};
+
+window.setInterval = function (callback, delay) {
+    if (typeof callback != 'function') {
+        throw new TypeError();
+    }
+    if (typeof delay != 'number') {
+        throw new TypeError();
+    }
+
+    var aArgs = aArgs;
+
+    setTimeout(delay, function () {
+        callback.apply(this, aArgs);
+        return setInterval(callback, delay);
+    });
+};
+
+function freeze(delay, callback) {
+    var timeout = [];
+
+    return function () {
+        var aArgs = arguments;
+
+        timeout.push(setTimeout(delay, function () {
+            callback.apply(this, aArgs);
+        }));
+
+        if (timeout.length > 1) {
+            clearTimeout(timeout.pop());
+        }
+    }
+}
+function createPipe(originalFnc, args) {
+    var aArgs = arguments;
+    var result;
+
+    for (var i = 0; i < aArgs.length; i++) {
+        result = args[0].apply(this, args);
+    }
+
+    originalFnc.call(this, result);
+}
+function originalFnc(string) {
+    var aString = string.split(' ');
+    for (var i = 0; i < aString.length; i++) {
+        aString[i] = aString[i].charAt(0).toUpperCase() + aString[i].slice(1)
+    }
+    string = aString.join(' ');
+
+    return string;
+}
+
+function filterDigits(string) {
+
+    return string.replace(/[0-9]/g, '');
+}
+
+function filterSpecial(string) {
+
+    return string.replace(/[\!\@\#\$\%\^\&\*\(\)\+\=]*/g, '');
+}
+
+function filterWhiteSpaces(string) {
+
+    return string.replace(/\s+/g," ");
+}
